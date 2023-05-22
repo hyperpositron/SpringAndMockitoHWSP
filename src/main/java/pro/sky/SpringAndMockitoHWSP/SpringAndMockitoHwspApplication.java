9f8ffaeda2d15b2caf72694e2f364f6962aae067
Александр <hyperpositron@gmail.com>
2023-05-22 16:09:56 +0200

    first commit

---
+++ src/main/java/pro/sky/SpringAndMockitoHWSP/SpringAndMockitoHwspApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.SpringAndMockitoHWSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndMockitoHwspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndMockitoHwspApplication.class, args);
+	}
+
+}
